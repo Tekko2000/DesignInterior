dfb194e8d2729c6874c585d21d550e6f520df5b7
Tekko2000 <ot1977@gmx.net>
2021-01-27 21:49:38 +0100

    DesignInterior

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function submitConfirmation() {
+    console.log("it works")
+    console.log(document.getElementById("input_Email").value)
+    console.log(document.getElementById("input_Name").value)
+    console.log(document.getElementById("input_Text").value)
+}
